4dc4964faeb620aa17850008e49706308c69261a
Ian Lozinski <ian.lozinski@gmail.com>
2013-10-06 11:04:34 +0000

    worked log in

--- worker.js
+++ worker.js
@@ -1,21 +1,28 @@
-var Browser = require('zombie');
-
-var browser = new Browser();
-
-
-function logIn() {
-  browser.visit("https://www.pinterest.com/login/", function() {
-    console.log(browser.text("title"));
-  
-    browser
-      .fill("email", "mitpindb@gmail.com")
-      .fill("password", "passwordy");
+var casper = require('casper').create({
+    verbose: true, 
+    logLevel: 'debug',
+    pageSettings: {
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+    }
+});
+ 
+var url = 'https://www.pinterest.com/login/';
+ 
+casper.start(url, function() {
+  // search for 'casperjs' from google form
+  console.log("page loaded");
+  this.fill('.loginForm', { 
+    username_or_email: 'mitpindb@gmail.com', 
+    password:  'passwordy'
   });
-}
-
-function upload(file) {
-  browser.visit('http://www.pinterest.com/mitpindb/') {
-    browser.clickLink("View Cart", function() {
-    });
-  }
-}
+ 
+  casper.thenClick('.formFooterButtons button');
+ 
+  casper.waitForUrl(/http:\/\/www\.pinterest\.com\/$/, function(){
+    console.log("Page Title " + document.title);
+    casper.debugHTML('.profileName');
+  });
+});
+ 
+ 
+casper.run();
